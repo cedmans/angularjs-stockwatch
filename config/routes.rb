24bdcbd18aeea187ae0e97fe0d39ee7d4918f1b7
Cedric Wienold <cwienold@gmail.com>
2014-04-19 15:21:06 -0700

    Create stock model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Stockwatch::Application.routes.draw do
+  resources :stocks
+
   root 'watchlist#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
